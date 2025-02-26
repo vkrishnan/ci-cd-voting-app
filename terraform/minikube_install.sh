9db1ac472eeb454ae7abc031432b43ae5c3a1121
Vinay Krishnan <vinay@Vinays-MacBook-Air.local>
2025-02-26 21:59:50 +1100

    fixed userdata bash script

--- terraform/minikube_install.sh
+++ terraform/minikube_install.sh
@@ -1,22 +1,29 @@
 #!/bin/bash
 
+# Update package repositories
+apt-get update -y
+
 # Install dependencies
-apt-get update
-apt-get install -y apt-transport-https curl gnupg
+apt-get install -y curl wget apt-transport-https
 
-# Add Kubernetes apt repository
-curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
-echo "deb https://apt.kubernetes.io/ kubernetes-xenial main" | tee /etc/apt/sources.list.d/kubernetes.list
+# Add Kubernetes apt repository (CORRECTED)
+curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
 
+# Update package repositories AGAIN after adding the Kubernetes repo
+install minikube-linux-amd64 /usr/local/bin/minikube
+
+curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl
 # Install Minikube and kubectl
-apt-get update
-apt-get install -y minikube kubectl
+chmod +x kubectl
+mv kubectl /usr/local/bin/
 
-# Start Minikube (using the private instance's IP as the control plane)
-MINIKUBE_CONTROL_PLANE_IP=$(curl -s http://169.254.169.254/latest/meta-data/local-ipv4)  #Get private IP
-minikube start --driver=none --kubernetes-version=v1.27.3 --control-plane-ip $MINIKUBE_CONTROL_PLANE_IP --extra-config=kubelet.cgroupDriver=systemd --apiserver-ips=$MINIKUBE_CONTROL_PLANE_IP
+# Get private IP
+PRIVATE_IP=$(curl -s http://169.254.169.254/latest/meta-data/local-ipv4)
 
-# Configure kubectl to use Minikube context
+# Start Minikube
+minikube start --driver=none --kubernetes-version=v1.27.3 --control-plane-ip $PRIVATE_IP --extra-config=kubelet.cgroupDriver=systemd --apiserver-ips=$PRIVATE_IP
+
+# Configure kubectl
 eval $(minikube docker-env)
 minikube config use minikube
 kubectl config use-context minikube
